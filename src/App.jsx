107ed888cceed93dac777a275b50b9b53dbdf7c0
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-02-18 12:25:41 +0600

    design card

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Cart from "./Components/Cart"
 
 
 function App() {
@@ -5,7 +6,7 @@ function App() {
 
   return (
     <>
-      
+      <Cart/>
     </>
   )
 }
---
+++ src/Components/Cart.jsx
@@ -0,0 +1,42 @@
+import p1 from '/images/p1.png'
+function Cart() {
+    return (
+        <section className=" max-w-container mx-auto">
+           <div className=' flex flex-wrap'>
+           <div>
+       <img src={p1} alt="" />
+       <h2 className=' text-center'>AMD Athlon Laptop</h2>
+       <p className=' w-[280px] text-[13px]'>HP 15s-eq1578AU AMD Athlon Silver 3050U 8GB 256GB SSD 15.6 Inch FHD Display Silver Laptop</p>
+       <p  className=' text-center'>46,500</p>
+            </div>
+
+            <div>
+       <img src={p1} alt="" />
+       <h2 className=' text-center'>AMD Athlon Laptop</h2>
+       <p className=' w-[280px] text-[13px]'>HP 15s-eq1578AU AMD Athlon Silver 3050U 8GB 256GB SSD 15.6 Inch FHD Display Silver Laptop</p>
+       <p  className=' text-center'>46,500</p>
+            </div>
+            <div>
+       <img src={p1} alt="" />
+       <h2 className=' text-center'>AMD Athlon Laptop</h2>
+       <p className=' w-[280px] text-[13px]'>HP 15s-eq1578AU AMD Athlon Silver 3050U 8GB 256GB SSD 15.6 Inch FHD Display Silver Laptop</p>
+       <p  className=' text-center'>46,500</p>
+            </div>
+            <div>
+       <img src={p1} alt="" />
+       <h2 className=' text-center'>AMD Athlon Laptop</h2>
+       <p className=' w-[280px] text-[13px]'>HP 15s-eq1578AU AMD Athlon Silver 3050U 8GB 256GB SSD 15.6 Inch FHD Display Silver Laptop</p>
+       <p  className=' text-center'>46,500</p>
+            </div>
+            <div>
+       <img src={p1} alt="" />
+       <h2 className=' text-center'>AMD Athlon Laptop</h2>
+       <p className=' w-[280px] text-[13px]'>HP 15s-eq1578AU AMD Athlon Silver 3050U 8GB 256GB SSD 15.6 Inch FHD Display Silver Laptop</p>
+       <p  className=' text-center'>46,500</p>
+            </div>
+           </div>
+        </section>
+    )
+}
+
+export default Cart
