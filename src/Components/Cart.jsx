56004387467a4fc330947889bd30cfeb640281a4
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-02-18 12:44:17 +0600

    design cart

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,40 +1,22 @@
 import p1 from '/images/p1.png'
 function Cart() {
     return (
-        <section className=" max-w-container mx-auto">
-           <div className=' flex flex-wrap'>
-           <div>
-       <img src={p1} alt="" />
+        <section className=" bg-[#f2f2f2] ">
+          <div className=' max-w-container mx-auto pt-3'>
+            <h2 className=' px-3 py-1 bg-black text-white w-[100px]  rounded-md mb-6  '>Computing</h2>
+          <div className=' flex flex-wrap gap-y-6 gap-x-5 '>
+           <div className=' group bg-white overflow-hidden'>
+      <div>
+      <img src={p1} alt=""  className='w-full group-hover:scale-105 duration-500 object-cover object-center'/>
+      </div>
        <h2 className=' text-center'>AMD Athlon Laptop</h2>
-       <p className=' w-[280px] text-[13px]'>HP 15s-eq1578AU AMD Athlon Silver 3050U 8GB 256GB SSD 15.6 Inch FHD Display Silver Laptop</p>
+       <p className=' w-[300px] text-[13px] px-3'>HP 15s-eq1578AU AMD Athlon Silver 3050U 8GB 256GB SSD 15.6 Inch FHD Display Silver Laptop</p>
        <p  className=' text-center'>46,500</p>
             </div>
-
-            <div>
-       <img src={p1} alt="" />
-       <h2 className=' text-center'>AMD Athlon Laptop</h2>
-       <p className=' w-[280px] text-[13px]'>HP 15s-eq1578AU AMD Athlon Silver 3050U 8GB 256GB SSD 15.6 Inch FHD Display Silver Laptop</p>
-       <p  className=' text-center'>46,500</p>
-            </div>
-            <div>
-       <img src={p1} alt="" />
-       <h2 className=' text-center'>AMD Athlon Laptop</h2>
-       <p className=' w-[280px] text-[13px]'>HP 15s-eq1578AU AMD Athlon Silver 3050U 8GB 256GB SSD 15.6 Inch FHD Display Silver Laptop</p>
-       <p  className=' text-center'>46,500</p>
-            </div>
-            <div>
-       <img src={p1} alt="" />
-       <h2 className=' text-center'>AMD Athlon Laptop</h2>
-       <p className=' w-[280px] text-[13px]'>HP 15s-eq1578AU AMD Athlon Silver 3050U 8GB 256GB SSD 15.6 Inch FHD Display Silver Laptop</p>
-       <p  className=' text-center'>46,500</p>
-            </div>
-            <div>
-       <img src={p1} alt="" />
-       <h2 className=' text-center'>AMD Athlon Laptop</h2>
-       <p className=' w-[280px] text-[13px]'>HP 15s-eq1578AU AMD Athlon Silver 3050U 8GB 256GB SSD 15.6 Inch FHD Display Silver Laptop</p>
-       <p  className=' text-center'>46,500</p>
-            </div>
+           
+            
            </div>
+          </div>
         </section>
     )
 }
