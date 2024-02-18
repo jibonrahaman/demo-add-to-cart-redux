91a49cc1b18450c6d8063a923260c5cdabcf62e6
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-02-18 19:29:27 +0600

    cart data mapping

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,18 +1,27 @@
+import { useState } from 'react'
 import p1 from '/images/p1.png'
+import data from '../Data'
+
 function Cart() {
+  const [show,setShow] =useState(data)
     return (
         <section className=" bg-[#f2f2f2] ">
           <div className=' max-w-container mx-auto pt-3'>
             <h2 className=' px-3 py-1 bg-black text-white w-[100px]  rounded-md mb-6  '>Computing</h2>
-          <div className=' flex flex-wrap gap-y-6 gap-x-5 '>
-           <div className=' group bg-white overflow-hidden'>
-      <div>
-      <img src={p1} alt=""  className='w-full group-hover:scale-105 duration-500 object-cover object-center'/>
-      </div>
-       <h2 className=' text-center'>AMD Athlon Laptop</h2>
-       <p className=' w-[300px] text-[13px] px-3'>HP 15s-eq1578AU AMD Athlon Silver 3050U 8GB 256GB SSD 15.6 Inch FHD Display Silver Laptop</p>
-       <p  className=' text-center'>46,500</p>
-            </div>
+          <div className=' flex flex-wrap gap-y-6 gap-x-10 '>
+          {
+            show.map((item)=>{
+              const {id,imgs,name,description,price} =item
+              return <div key={id} className=' mt-5 mb-10 group bg-white overflow-hidden'>
+              <div>
+              <img src={imgs} alt=""  className='w-full group-hover:scale-105 duration-500 object-cover object-center'/>
+              </div>
+               <h2 className=' text-center'>{name}</h2>
+               <p className=' w-[300px] text-[13px] px-3'>{description}</p>
+               <p  className=' text-center'>{price}</p>
+                    </div>
+            })
+          }
            
             
            </div>
--- src/Data.js
+++ src/Data.js
@@ -2,6 +2,38 @@ const data = [
     {
   "id" :1,
   "imgs" : '/images/p1.png',
-    }
+  "name" : "AMD Athlon Laptop",
+  "description" : "HP 15s-eq1578AU AMD Athlon Silver 3050U 8GB 256GB SSD 15.6 Inch FHD Display Silver Laptop",
+  "price" : "46,500",
+    },
+    {
+        "id" :2,
+        "imgs" : '/images/p2.png',
+        "name" : "Apple iMac",
+        "description" : "Apple iMac (Late 2023) 24 Inch 4.5K Retina Di",
+        "price" : "232,000",
+          },
+          {
+            "id" :3,
+            "imgs" : '/images/p3.png',
+            "name" : "Billing Counter",
+            "description" : "Ryans POS Billing Counter Package RPBC-02 (1x Full Set PC, 1x Barcode Scanner, 1x POS Printer, 1x Cash Drawer) With Dual Monitor #RPBC-02",
+            "price" : "59,500",
+              },
+               {
+            "id" :4,
+            "imgs" : '/images/p4.png',
+            "name" : "Surface Studio Laptop",
+            "description" : "Ryans POS Billing Counter Package RPBC-02 (1x Full Set PC, 1x Barcode Scanner, 1x POS Printer, 1x Cash Drawer) With Dual Monitor #RPBC-02",
+            "price" : "159,500",
+              },  
+               {
+            "id" :5,
+            "imgs" : '/images/p5.png',
+            "name" : "Legion Gaming",
+            "description" : "Lenovo Legion 5i Pro 16IAH7H Intel Core i7 12 ",
+            "price" : "259,500",
+              },
+
 ]
  export default data
